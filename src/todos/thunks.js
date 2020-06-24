2f976fa3eab6b047c3ee7c2c2783b02be9d2b8d1
Alexander Shvyrev <shurik_stv@yahoo.com>
2020-06-24 10:57:32 -0400

    initial

---
+++ src/todos/thunks.js
@@ -0,0 +1,67 @@
+import {
+    createTodo,
+    removeTodo,
+    loadTodosInProgress,
+    loadTodosSuccess,
+    loadTodosFailure,
+    markTodoAsCompleted
+} from './actions';
+
+export const loadTodos = () => async (dispatch, getState) => {
+    try {
+        dispatch(loadTodosInProgress());
+        const response = await fetch('http://localhost:8080/todos-delay');
+        const todos = await response.json();
+
+        dispatch(loadTodosSuccess(todos));
+    } catch (e) {
+        dispatch(loadTodosFailure());
+        dispatch(displayAlert(e));
+    }
+}
+
+export const addTodoRequest = text => async dispatch => {
+    try {
+
+        const body = JSON.stringify({ text })
+        const response = await fetch('http://localhost:8080/todos', {
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            method: 'post',
+            body
+        })
+        const todo = await response.json()
+        dispatch(createTodo(todo))
+    } catch (error) {
+        dispatch(displayAlert(error))
+    }
+}
+export const removeTodoRequest = id => async dispatch => {
+    try {
+        const response = await fetch(`http://localhost:8080/todos/${id}`, {
+            method: 'delete'
+        })
+        const removedTodo = await response.json()
+        dispatch(removeTodo(removedTodo))
+    } catch (error) {
+        dispatch(displayAlert(error))
+
+    }
+}
+export const markTodoAsCompletedRequest = id => async dispatch => {
+    try {
+        const response = await fetch(`http://localhost:8080/todos/${id}/completed`, {
+            method: 'post'
+        })
+        const completedTodo = await response.json()
+        dispatch(markTodoAsCompleted(completedTodo))
+    } catch (error) {
+        dispatch(displayAlert(error))
+
+    }
+}
+
+export const displayAlert = text => () => {
+    alert(text);
+};
